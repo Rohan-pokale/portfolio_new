811c34884617e880b94224fd40a828bab468eeaf
Rohan-pokale <pokalerohan06@gmail.com>
2026-02-01 00:27:17 +0530

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,21 +15,20 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/favicon-32x32.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/favicon-32x32.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/favicon-32x32.png',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
-  },
-  generator: 'v0.app'
+    apple: '/favicon-32x32.png',
+  }
 }
 
 export default function RootLayout({
